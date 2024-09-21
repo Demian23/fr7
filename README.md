582122441850565f5211a91a4d59de005b9610d9
Demian23 <smegor55@gmail.com>
2024-09-21 19:26:15 +0300

    Initial commit. Base packages installed.

---
+++ README.md
@@ -0,0 +1,4 @@
+# fr7
+## FilmRate7
+So much time has passed and that's it: seventh reincarnation of Film Rate.
+This time written on typescript
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send("HAHAHAH!")
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
